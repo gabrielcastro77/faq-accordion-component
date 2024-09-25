b3616c4b37acc141db699184c86365442d9d42e1
Gabriel Castro <gabrielsilvadecastro777@gmail.com>
2024-09-25 16:36:42 -0300

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bem-vindo! 👋
+
+Obrigado por conferir esse desafio.
+
+Para desenvolver esse projeto, eu atilizei as tecnologias HTML, CSS e JavaScript.
+
+## Projeto - Componente de classificação interativo
+
+Meu desafio é construir este FAQ acordeão e fazê-lo parecer o mais próximo possível do design.
+
+Seus usuários devem ser capazes de:
+
+- Ocultar/Mostrar a resposta a uma pergunta quando a pergunta for clicada.
+- Navegar pelas perguntas e ocultar/mostrar respostas usando apenas a navegação do teclado.
+- Visualizar o layout ideal para a interface dependendo do tamanho da tela do dispositivo.
+- Ver os estados de foco e foco para todos os elementos interativos na página.
---
+++ script.js
@@ -0,0 +1,18 @@
+const accordion0 = document.getElementsByClassName("accordion")[0];
+const answer0 = document.getElementsByClassName("answer")[0]
+accordion0.addEventListener("click", function(){
+    answer0.classList.toggle("answer-toggle")
+})
+
+const accordion1 = document.getElementsByClassName("accordion")[1];
+const answer1 = document.getElementsByClassName("answer")[1]
+accordion1.addEventListener("click", function(){
+    answer1.classList.toggle("answer-toggle")
+})
+
+const accordion2 = document.getElementsByClassName("accordion")[2];
+const answer2 = document.getElementsByClassName("answer")[2]
+accordion2.addEventListener("click", function(){
+    answer2.classList.toggle("answer-toggle")
+})
+
